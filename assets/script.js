229710e6c2fd1926ada2d83081a57e954b22003e
JamesAChavesForero <javeschames2024@gmail.com>
2024-09-23 16:58:28 -0500

    started developing dynamic forms

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,15 @@
+var images = {
+  promos: {
+    edan: "5% off",
+  },
+};
 var PopUpObject = {
   freeShipping1: {
-    img: "./imgs/img.svg",
     smallText:
       "Purchase over <strong>150$</strong> Replacement parts & Recieve",
     bigText: "Free Shipping",
   },
   freeShipping2: {
-    img: "./imgs/img.svg",
     text1: ["Reduced shipping Rates Available for ", "black"],
     text2: ["Hospital Beds | procedure Chairs | Stretchers", "red"],
     text3: ["Reduced shipping Rates Available for ", "black"],
@@ -14,7 +17,6 @@ var PopUpObject = {
     btn: ["Shop Now", "white", "red"],
   },
   reduceShippingStates: {
-    img: "./imgs/img.svg",
     imgText: ["Shopping in ", "White"],
     bigText: ["Reduced shipping Rates Available", "black"],
     smallText: ["Hospital Beds | procedure Chairs | Stretchers", "red"],
@@ -39,22 +41,16 @@ var formObject = {
   },
 };
 
-console.log(
-  "connected",
-  PopUpObject["reduceShippingStates"]["bigText"][0],
-  PopUpObject["reduceShippingStates"]["bigText"][1]
-);
-
 const stateBasedPopUp = async () => {
   let url = "https://ipinfo.io/json?token=39dd58180d085e";
   let response = await fetch(url);
-  let data = await response.json()
-  console.log(data)
+  let data = await response.json();
+  console.log(data);
   let aside = document.createElement("aside");
   aside.id = "imgContainer";
   aside.innerHTML = `
-        <img src="./assets/imgs/illinois.jpg" />
-        <span class="imgText">Shipping In Illinois ?</span>
+        <img src="./assets/imgs/${data.region}.jpg" />
+        <span class="imgText">Shipping In <br> ${data.city}, ${data.region}?</span>
         <div class="infoBox">
             <span class="bigText">
             ${PopUpObject["reduceShippingStates"]["bigText"][0]}
@@ -69,5 +65,107 @@ const stateBasedPopUp = async () => {
     `;
   document.getElementById("module").append(aside);
 };
+
+// provides fields and webhook
+const formBasedPopUp = {
+  "Contact Us": {
+    webhook: "https://",
+    inputFields: [
+      "First Name",
+      "Last Name",
+      "Email",
+      "Order Number",
+      "Company Name",
+      "RMA Number",
+    ],
+    textArea: ["Comments"],
+    buttonText: "Contact Us",
+  },
+  "Purchase Order": {
+    webhook: "https://",
+    fields: [
+      "Name",
+      "Address",
+      "Email",
+      "Address Line 2",
+      "City",
+      "State",
+      "Postal / Zip Code",
+      "Country",
+      "Phone Number",
+      "Email",
+      "Business Name",
+    ],
+    textArea: ["Comments"],
+    buttonText: "Submit",
+  },
+  "Make an Offer": {
+    webhook: "https://",
+    fields: ["Make your Offer", "First Name", "Email", "Phone Number"],
+    buttonText: "Send",
+  },
+  "Sell to Us": {
+    webhook: "https://",
+    fields: [
+      "First Name",
+      "Last Name",
+      "Phone Number",
+      "Email",
+      "Order Number",
+      "Company Name",
+      "Comments",
+    ],
+    textArea: ["Comments / Questions"],
+    buttonText: "Submit",
+  },
+  "Replacement Part": {
+    webhook: "https://",
+    fields: [
+      "Name of Product",
+      "OEM Part Number",
+      "First Name",
+      "Last Name",
+      "Phone Number",
+      "Comments / Question",
+      "Email",
+    ],
+    textArea: ["Comments / Questions"],
+    buttonText: "Submit",
+  },
+  "Price Match": {
+    webhook: "https://",
+    fields: [
+      "First Name",
+      "Last Name",
+      "Phone Number",
+      "Email",
+      "Product",
+      "Brand",
+      "Model Number",
+      "Color",
+    ],
+    textArea: ["Comments / Questions"],
+    buttonText: "Submit",
+  },
+  "Color Chart": {
+    webhook: "https://",
+    fields: [
+      "Name",
+      "Address",
+      "Address Line 2",
+      "City",
+      "State",
+      "Postal / Zip Code",
+      "Country",
+      "Phone Number",
+      "Email",
+      "Business Name",
+    ],
+    textArea: ["Comments / Questions"],
+    buttonText: "Submit",
+  },
+};
+
+const promoBasedPopUp = {};
 
 stateBasedPopUp();
